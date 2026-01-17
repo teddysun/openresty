4c23143637931848e00fe85cf61608d4e88e6da8
Teddysun <i@teddysun.com>
2026-01-17 22:22:16 +0900

    Update build-win32.sh

--- util/build-win32.sh
+++ util/build-win32.sh
@@ -3,29 +3,26 @@
 PCRE=pcre2-10.47
 ZLIB=zlib-1.3.1
 OPENSSL=openssl-3.5.4
-JOBS=12
 
 if [ ! -f ../$OPENSSL.tar.gz ]; then wget -O ../$OPENSSL.tar.gz https://github.com/openssl/openssl/releases/download/$OPENSSL/$OPENSSL.tar.gz; fi
-if [ ! -f ../$ZLIB.tar.gz ]; then wget -O ../$ZLIB.tar.gz http://zlib.net/$ZLIB.tar.gz; fi
+if [ ! -f ../$ZLIB.tar.gz ]; then wget -O ../$ZLIB.tar.gz http://www.zlib.net/$ZLIB.tar.gz; fi
 if [ ! -f ../$PCRE.tar.gz ]; then wget -O ../$PCRE.tar.gz https://github.com/PCRE2Project/pcre2/releases/download/$PCRE/$PCRE.tar.gz; fi
 
 rm -rf objs || exit 1
 mkdir -p objs/lib || exit 1
 cd objs/lib || exit 1
 ls ../../..
-tar -xf ../../../$OPENSSL.tar.gz || exit 1
-tar -xf ../../../$ZLIB.tar.gz || exit 1
-tar -xf ../../../$PCRE.tar.gz || exit 1
+tar -xzf ../../../$OPENSSL.tar.gz || exit 1
+tar -xzf ../../../$ZLIB.tar.gz || exit 1
+tar -xzf ../../../$PCRE.tar.gz || exit 1
 cd ../..
 
 cd objs/lib/$OPENSSL || exit 1
 patch -p1 < ../../../patches/openssl-3.5.4-sess_set_get_cb_yield.patch || exit 1
 cd ../../..
 
-    #--with-openssl-opt="no-asm" \
-
 ./configure \
-    --with-cc=gcc \
+    --with-cc=cc \
     --prefix= \
     --with-cc-opt='-DFD_SETSIZE=1024' \
     --sbin-path=nginx.exe \
@@ -33,12 +30,12 @@ cd ../../..
     --without-http_rds_json_module \
     --without-http_rds_csv_module \
     --without-lua_rds_parser \
+    --with-mail \
     --with-stream \
     --with-stream_realip_module \
     --with-stream_ssl_module \
     --with-stream_ssl_preread_module \
     --with-http_v2_module \
-    --with-http_v3_module \
     --without-mail_pop3_module \
     --without-mail_imap_module \
     --without-mail_smtp_module \
@@ -60,8 +57,10 @@ cd ../../..
     --with-pcre=objs/lib/$PCRE \
     --with-zlib=objs/lib/$ZLIB \
     --with-openssl=objs/lib/$OPENSSL \
-    -j$JOBS || exit 1
+    --with-openssl-opt='no-asm no-tests no-makedepend -D_WIN32_WINNT=0x0501' \
+    --with-http_ssl_module \
+    --with-mail_ssl_module \
+    -j`nproc` || exit 1
 
-make -j$JOBS || exit 1
-exec make install
-
+make -j`nproc` || exit 1
+make install
